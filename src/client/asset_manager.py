627cc03b845775eedd1610c35206369146726db9
Mateusz Grzonka <alpinus4@gmail.com>
2022-04-17 17:18:43 +0200

    Use directory defined in config for loading assets

--- src/client/asset_manager.py
+++ src/client/asset_manager.py
@@ -6,16 +6,16 @@ import config
 
 class AssetManager:
     def __init__(self):
-        self.board_img = pygame.image.load(path.join('assets', 'board.png'))
-        self.striker_img = pygame.image.load(path.join('assets', 'striker.png'))
-        self.puck_img = pygame.image.load(path.join('assets', 'puck.png'))
-        self.result_bg_big_img = pygame.image.load(path.join('assets', 'result_bg_big.png'))
-        self.result_bg_small_img = pygame.image.load(path.join('assets', 'result_bg_small.png'))
-        self.colon_img = pygame.image.load(path.join('assets', 'colon.png'))
+        self.board_img = pygame.image.load(path.join(config.ASSETS_FOLDER, 'board.png'))
+        self.striker_img = pygame.image.load(path.join(config.ASSETS_FOLDER, 'striker.png'))
+        self.puck_img = pygame.image.load(path.join(config.ASSETS_FOLDER, 'puck.png'))
+        self.result_bg_big_img = pygame.image.load(path.join(config.ASSETS_FOLDER, 'result_bg_big.png'))
+        self.result_bg_small_img = pygame.image.load(path.join(config.ASSETS_FOLDER, 'result_bg_small.png'))
+        self.colon_img = pygame.image.load(path.join(config.ASSETS_FOLDER, 'colon.png'))
 
         self.numbers_img = []
         for i in range(10):
-            self.numbers_img.append(pygame.image.load(path.join('assets', str(i) + '.png')))
+            self.numbers_img.append(pygame.image.load(path.join(config.ASSETS_FOLDER, str(i) + '.png')))
 
     @staticmethod
     def scale_img(img):
