bacb3c901366b90d83978041568a2147873f4b99
FilipSzweda <filip.szweda1@gmail.com>
2022-06-03 14:54:58 +0200

    Fix erratic player movement with mouse out of bounds

--- src/client/striker.py
+++ src/client/striker.py
@@ -21,24 +21,32 @@ class Striker(PhysicsGameObject):
         new_pos = self.get_position()
         return math.sqrt((old_pos[0] - new_pos[0]) ** 2 + (old_pos[1] - new_pos[1]) ** 2)
 
-    def change_by_mouse_position(self, m_pos, movement_area):
+    def change_by_mouse_position(self, m_pos, p_movement_area):
         m_rect = pygame.Rect(m_pos[0] - self.rect.w / 2, m_pos[1] - self.rect.h / 2, self.rect.w, self.rect.h)
-        if movement_area.contains(m_rect):
+        if p_movement_area.contains(m_rect):
             self.set_position(m_pos)
         else:
-            m_x_rect = pygame.Rect(m_pos[0] - self.rect.w / 2, self.rect.top, self.rect.w, self.rect.h)
-            if movement_area.contains(m_x_rect):
-                self.set_position((m_x_rect.centerx, m_x_rect.centery))
-            m_y_rect = pygame.Rect(self.rect.left, m_pos[1] - self.rect.h / 2, self.rect.w, self.rect.h)
-            if movement_area.contains(m_y_rect):
-                self.set_position((m_y_rect.centerx, m_y_rect.centery))
+            new_x, new_y = self.body.position
+            if m_rect.x + m_rect.width > p_movement_area.x + p_movement_area.width:
+                new_x = p_movement_area.x + p_movement_area.width - m_rect.width / 2
+            elif m_rect.x < p_movement_area.x:
+                new_x = p_movement_area.x + m_rect.width / 2
+            else:
+                new_x = m_rect.x + m_rect.width / 2
+            if m_rect.y < p_movement_area.y:
+                new_y = p_movement_area.y + self.rect.height / 2
+            elif m_rect.y + m_rect.height > p_movement_area.y + p_movement_area.height:
+                new_y = p_movement_area.y + p_movement_area.height - m_rect.height / 2
+            else:
+                new_y = m_rect.y + m_rect.height / 2
+            self.body.position = new_x, new_y
 
-    def check_player_input(self, player_input, movement_area):
+    def check_player_input(self, p_input, p_movement_area):
         old_pos = self.get_position()
-        if player_input.dragging:
+        if p_input.dragging:
             m_pos = pygame.mouse.get_pos()
             if self.is_dragged or self.rect.collidepoint(m_pos):
-                self.change_by_mouse_position(m_pos, movement_area)
+                self.change_by_mouse_position(m_pos, p_movement_area)
             if self.rect.collidepoint(m_pos):
                 self.is_dragged = True
         else:
